261e8b7ebd67102e4fc108e96e87a13af9d12e0a
Stin <krikarlss@gmail.com>
2021-04-30 20:11:32 +0200

    Restart on project

---
+++ public/javascripts/responsible.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+
+    console.log('Client-side code running');
+    $('#firstButton').on("click", (evt) => {
+        console.log("Clicked submitButton");
+    });
+
+
+});
+
+
+
