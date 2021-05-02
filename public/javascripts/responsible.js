56154645cce4aee67d4e660d987db7677cbbd0e4
Stin <krikarlss@gmail.com>
2021-05-02 17:05:51 +0200

    Rename button, change button position

--- public/javascripts/responsible.js
+++ public/javascripts/responsible.js
@@ -171,7 +171,7 @@ let displayTempChart = () => {
 $(document).ready(() => {
     console.log('Client-side code running');
 
-    $('#firstButton').on("click", (evt) => {
+    $('#snapshotBtn').on("click", (evt) => {
         console.log("Clicked submitButton");
         console.log("Redirect to map");
         window.location.href = "/map";
