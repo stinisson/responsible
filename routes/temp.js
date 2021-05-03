0cd01c5f6a152b2724acab26e660bd3cf5592692
Stin <krikarlss@gmail.com>
2021-05-03 22:52:12 +0200

    Start on telegram bot functionality: ask bot for latest temp reading

--- routes/temp.js
+++ routes/temp.js
@@ -9,18 +9,43 @@ console.log(botCredentials, 'the json obj');
 const MongoClient = mongodb.MongoClient;
 const dbURL = "mongodb://localhost";
 
-router.get('/', (req, res) => {
+// TODO -polling_error -getTemp -> wrong reading
 
+function botSendTemperature(bot, sensorData) {
+    bot.on('message', (msg) => {
+        const chatId = msg.chat.id
+        if (msg.text === 'getTemp') {
+            let latestReading = new Date(sensorData.timestamp * 1000).toString().substr(0, 21);
+            bot.sendMessage(chatId, "Temperature: " + sensorData.lastReadTemp + "°C.\n" + latestReading);
+        }
+    });
+}
+
+function telegramBot() {
     const token = botCredentials.accessToken
     const bot = new TelegramBot(token, { polling: true })
     bot.onText(/\/echo (.+)/, (msg, match) => {
 
         const chatId = msg.chat.id
         const resp = match[1]
-
         bot.sendMessage(chatId, resp)
+    });
+
+    let users = []
+    bot.onText(/\/register/, (msg, match) => {
+        const chatId = msg.chat.id
+        users.push(chatId)
+        console.log('user registered')
+        bot.sendMessage(chatId, 'Done.')
     })
 
+    return bot;
+}
+
+router.get('/', (req, res) => {
+    let sensorData = {};
+
+    let bot = telegramBot();
 
     MongoClient.connect(dbURL, {useUnifiedTopology: true}, (err, client) => {
         if (err) throw err;
@@ -31,14 +56,15 @@ router.get('/', (req, res) => {
             tempReading.find( {}).project( { "temperature": { "$slice": -10 } }).toArray((err, tempData) => {
                 if (err) throw err;
                 else {
-                    console.log("Reading from Mongo: ")
-                    tempData.forEach(element => {
-                        console.log(element['temperature'][0]['timestamp']);
-                        console.log(element['temperature'][0]['degreesCelsius']);
-                    });
+                    let numTempReadings = tempData[0]['temperature'].length;
+                    sensorData = {"lastReadTemp": tempData[0]['temperature'][numTempReadings - 1]['degreesCelsius'],
+                        timestamp: tempData[0]['temperature'][numTempReadings - 1]['timestamp']};
+                    console.log("sensor data: " + sensorData.lastReadTemp + " " + sensorData.timestamp);
+
+                    res.json({result: tempData});
+                    botSendTemperature(bot, sensorData)
                 }
                 client.close();
-                res.json({result: tempData});
             });
         }
     });
